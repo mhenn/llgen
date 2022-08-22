ea57e5042ab65617c6842b5f1031f4452c31dc85
Mike Henning <henning@fh-aachen.de>
2022-08-22 19:23:01 +0200

    fixed initialization

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,5 @@
 
-use rand::Rng;
-//use crate::grammar::*;
+use rand::{Rng, distributions::Uniform};
 use crate::population::*;
 
 
@@ -8,16 +7,16 @@ fn generate_random_codon() -> u8{
     rand::thread_rng().gen_range(0..=255)
 }
 
-fn generate_chromosome<T>(size: usize, generator: fn() -> T) -> Vec<T>
-    where T: Clone
+fn generate_chromosome<T>(size: i32, generator: fn() -> T) -> Vec<T>
 {
     (0..size).into_iter().map(|_| generator()).collect()
 }
 
-fn generate_rnd_chromosomes(min: i32, max: i32, size: usize) -> Vec<Vec<u8>>{
-    let mut rng =  rand::thread_rng();
+fn generate_rnd_chromosomes(min: i32, max: i32, size: i32, ) -> Vec<Vec<u8>>{
+    let dist = Uniform::new_inclusive(min,max);
+
     (0..size).into_iter()
-        .map(|_| (min..((rng.gen::<i32>() % max) + min)).into_iter().map(|_| rng.gen_range(0..=255)).collect())
+        .map(|_| generate_chromosome(rand::thread_rng().sample(dist),generate_random_codon ))
         .collect()
 }
 
@@ -28,26 +27,27 @@ fn generate_individuals(chromosomes: Vec<Vec<u8>>) -> Vec<Individual>{
         .collect()
 }
 
-pub fn rnd(min: i32, max: i32, pop_size: usize) -> Generation {
+pub fn rnd(min: i32, max: i32, pop_size: i32) -> Generation {
     Generation{individuals: generate_individuals(generate_rnd_chromosomes(min, max, pop_size)), count:0}
 }
 
 
 #[test]
-fn chromosomes_test(){
-    let size: usize = 15;
+fn chromosomes_rnd(){
+    let size: i32 = 15;
     let max: i32 = 10;
     let chromosomes = generate_rnd_chromosomes(1, max, size);
-    assert_eq!(chromosomes.len(), size );
-    let over_count = chromosomes.iter().fold(0, |acc, x| {print!("{:?} ", x.len()); if (x.len() as i32) <= max {acc} else { acc+1}});
+    assert_eq!(chromosomes.len() as i32, size );
+    let over_count = chromosomes.iter().fold(0, |acc, x| if (x.len() as i32) <= max {acc} else { acc+1});
     assert_eq!(over_count, 0);
 }
 
 #[test]
-fn chromosome_test(){
-    let size: usize = 15;
-    let mut rng =  rand::thread_rng();
+fn chromosome_rnd(){
+    let size: i32 = 15;
     let chromosome = generate_chromosome(size,  generate_random_codon);
-    println!("{:?}", chromosome);
-    assert_eq!(1, 1);
+    assert_eq!(chromosome.len() as i32, size );
 }
+
+
+
